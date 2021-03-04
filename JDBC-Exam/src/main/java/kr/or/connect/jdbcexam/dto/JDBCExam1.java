468243e6ad11079405795c8e26df17caa3b67e7c
dev-splin <dev.splin@gmail.com>
2021-03-04 13:48:52 +0900

    Delete JDBC Exam

--- JDBC-Exam/src/main/java/kr/or/connect/jdbcexam/dto/JDBCExam1.java
+++
@@ -1,9 +0,0 @@
-package kr.or.connect.jdbcexam.dto;
-
-public class JDBCExam1 {
-	public static void main(String[] args) {
-		RoleDao dao = new RoleDao();
-		Role role = dao.getRole(100);
-		System.out.println(role);
-	}
-}
